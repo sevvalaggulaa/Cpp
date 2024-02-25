be031b1abeb488a5f5d7932ef0867a7a1a0ff40e
sevval aggul <151607969+sevvalaggulaa@users.noreply.github.com>
2024-02-25 20:12:30 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	string isim= "sevval";
+	int yas= 20;
+	
+	cout<< "merhaba :)" << endl << "benim adim " << isim << endl;
+	cout<<"ben "<<yas<<" yasindayim."<< endl;
+	
+	return 0;
+}
