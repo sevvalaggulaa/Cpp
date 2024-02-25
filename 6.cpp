6aee70a683a2b95bd3413f9343057f8c6bc6d6ed
sevval aggul <151607969+sevvalaggulaa@users.noreply.github.com>
2024-02-25 20:14:56 +0300

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string isim;
+	cout << "lutfen adinizi yaziniz."<<endl;
+	cin >> isim;
+	cout << "merhaba " << isim << ", hosgeldin";
+	return 0;
+}
