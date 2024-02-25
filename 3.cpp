ae6d988ba9d7ae54366a55947808fa418af84df3
sevval aggul <151607969+sevvalaggulaa@users.noreply.github.com>
2024-02-25 20:13:47 +0300

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	string isim= "sevval aggul";
+	int dogumyili = 2003;
+	float notOrt= 3.48f;
+	double pi=3.1415236;
+	
+	cout <<"merhaba " <<isim<< endl;
+	cout<< "dogum yili= "<<dogumyili<<endl;
+	cout << "not ortalamasi = "<<notOrt<<endl;
+	
+	return 0;
+}
