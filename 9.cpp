a5878d8328967a75b75f64099f88e45f24912b55
sevval aggul <151607969+sevvalaggulaa@users.noreply.github.com>
2024-02-25 20:15:29 +0300

    Add files via upload

---
+++ 9.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main( ) {
+	
+	int sicaklik= 40;
+	
+	if (sicaklik>=35)
+	{
+		cout << "ortam sicak";
+	}
+	else 
+	{
+		cout << "ortam sicak degil";
+	}
+	return 0;
+}
