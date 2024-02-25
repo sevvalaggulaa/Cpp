bfaa29576efae12a54965211f333ecf3e9a9a222
sevval aggul <151607969+sevvalaggulaa@users.noreply.github.com>
2024-02-25 22:10:01 +0300

    Add files via upload

---
+++ 18.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int n;
+	
+	cout<<"lutfen bir sayi giriniz= ";
+	cin>>n;
+	
+    for(int i=0;i<=n;i++)
+    {
+    	if(i%2==0)
+    	{
+    		cout<<i<<endl;
+		}
+	}
+	return 0;
+}
