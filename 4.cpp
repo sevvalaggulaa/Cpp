3f4e02a177c8f6ca50b1c553287ba1537369ddc1
sevval aggul <151607969+sevvalaggulaa@users.noreply.github.com>
2024-02-25 20:14:07 +0300

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	bool sinifiGectiMi= true;
+	char harfNotu= 65;
+	
+	cout << "sinifi gectiniz mi= " << sinifiGectiMi << endl;
+	cout << "harf notu = " << harfNotu << endl;
+	
+	return 0;
+}
