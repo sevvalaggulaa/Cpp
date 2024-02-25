71bbebcea09408f1343df4a83517b2adfb9406ed
sevval aggul <151607969+sevvalaggulaa@users.noreply.github.com>
2024-02-25 20:22:40 +0300

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	
+	int number1= 156;
+	float number2=22.3;
+	double number3=14.0253;
+	char ch='H';
+	
+	cout << "1. variable: "<<number1<<endl;
+	cout << "2. variable: "<<number2<<endl;
+	cout << "3. variable: "<<number3<<endl;
+	cout << "4. variable: "<<ch<<endl;
+	
+	
+	return 0;
+}
