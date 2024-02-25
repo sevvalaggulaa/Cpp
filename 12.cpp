71d73eeae52965abf5bbe7028ec80251e6bd2dc3
sevval aggul <151607969+sevvalaggulaa@users.noreply.github.com>
2024-02-25 20:16:08 +0300

    Add files via upload

---
+++ 12.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x=1234;
+	int y=1233;
+	
+	if(x!=y)
+	{
+		cout << "yanlis sifre";
+	}
+	else
+	{
+		cout << "dogru sifre";
+	}
+	return 0;
+}
