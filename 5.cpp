efa99a3546045ec12f323cfd9611e71ab6354201
sevval aggul <151607969+sevvalaggulaa@users.noreply.github.com>
2024-02-25 20:14:39 +0300

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int quiz1= 72;
+	int quiz2= 51;
+	float quizOrt= (float)(quiz1+quiz2)/2;
+	cout << "quizlerin ortalamasi= "<< endl;
+	
+	return 0;
+}
